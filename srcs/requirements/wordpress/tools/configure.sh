9243c9cabb2603ee98830071856f1cc61f50803a
tratanat <tawan.rtn@gmail.com>
2022-08-20 10:38:46 +0700

    Working

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -4,7 +4,7 @@ while ! mariadb -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASS $
 	sleep 3
 done
 
-mkdir -p /var/www/html
+mkdir -p /var/www/html/wordpress
 chown -R www-data:www-data /var/www/*
 # chown -R 755 /var/www/*
 mkdir -p /run/php/
@@ -15,11 +15,11 @@ cd /tmp/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-cd /var/www/html
+cd /var/www/html/wordpress
 wp core download --allow-root
 wp config create --dbname=${WORDPRESS_DB_NAME} --dbhost=${WORDPRESS_DB_HOST} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASS} --allow-root
 echo "Created config at $(pwd)."
-wp core install --url=${INCEPTION_URL} --title=${INCEPTION_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL} --allow-root
+wp core install --url=${INCEPTION_URL}/wordpress --title=${INCEPTION_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASS} --admin_email=${WORDPRESS_EMAIL} --allow-root
 
 service php7.3-fpm start
 tail -f /dev/null
